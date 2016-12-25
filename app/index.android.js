3340589b828cf60658497485f85f0c019c142f0d
Drew Volz <volz.drew@gmail.com>
2016-12-24 23:40:11 -0500

    Moved android index to app folder

---
+++ app/index.android.js
@@ -0,0 +1,14 @@
+/**
+ * Lexington Traffic Cameras Android App
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+} from 'react-native';
+
+import HomeScene from './views/home-scene/home'
+
+'use strict';
+
+AppRegistry.registerComponent('LexingtonTrafficCameras', () => HomeScene)
